ebdc5b946fa15ed9e8cfcbe69c568e7ee5a244f6
Christian Aistleitner <christian@quelltextlich.at>
2020-06-22 12:53:40 +0200

    Add instructions on how to run the go demo
    Change-Id: I6167c3346355fbb021dc3e8bb2c695e044284d79

--- demo.go
+++ demo.go
@@ -1,5 +1,36 @@
 package main
 
+/*
+  For all of the following, adapt `gerrit.wikimedia.org` to the Gerrit
+  server you test against.
+
+  To run this demo, first install go.
+  For example on debian, run
+     sudo apt-get install golang
+
+  Then remove eventual pull/binaries that you might still be there
+  from previous runs:
+
+    rm -rf ~/go/src/gerrit.wikimedia.org/r/test/gerrit-ping
+    rm -rf ~/go/bin/gerrit-ping
+
+  Then fetch the sources from gerrit:
+
+     go get gerrit.wikimedia.org/r/test/gerrit-ping
+
+  (this will not print any confirmation)
+  And finally run:
+
+     go run gerrit.wikimedia.org/r/test/gerrit-ping
+
+  This should output:
+
+    demo works!
+
+  To clean up again, run the above `rm` commands.
+  Done!
+*/
+
 import "fmt"
 
 func main() {
